9094f5c59152838f2e387bb40487b19c360ac947
sanoyo <sa00380304yo@gmail.com>
2023-02-18 17:49:29 +0900

    yaml読み込んで、s3構築まで

--- cmd/storage.go
+++ cmd/storage.go
@@ -7,12 +7,20 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
+
+type S3Object struct {
+	Kind       string `yaml:"kind"`
+	BucketName string `yaml:"name"`
+}
 
 // storageCmd represents the storage command
 var storageCmd = &cobra.Command{
@@ -25,20 +33,31 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("storage called")
+		// yamlファイルに読み込み
+		s3Object := S3Object{}
+		b, _ := os.ReadFile("./sample/s3.yaml")
+		yaml.Unmarshal(b, &s3Object)
 
+		fmt.Printf("s3Object: %+v\n", s3Object)
+		fmt.Printf("Tons: %v\n", s3Object.BucketName)
+
+		// ローカルの認証情報を読み込む
 		cfg, err := config.LoadDefaultConfig(context.TODO())
 		if err != nil {
 			return err
 		}
 
-		// Create an Amazon S3 service client
+		// s3用のclientを作る
 		client := s3.NewFromConfig(cfg)
-
-		// Get the first page of results for ListObjectsV2 for a bucket
-		output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-			Bucket: aws.String("my-bucket"),
-		})
+		output, err := client.CreateBucket(
+			context.TODO(),
+			&s3.CreateBucketInput{
+				Bucket: aws.String(s3Object.BucketName),
+				CreateBucketConfiguration: &types.CreateBucketConfiguration{
+					LocationConstraint: types.BucketLocationConstraint(cfg.Region),
+				},
+			},
+		)
 		if err != nil {
 			return err
 		}
--- go.mod
+++ go.mod
@@ -26,4 +26,5 @@ require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
