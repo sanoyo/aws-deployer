b6cdcc7476a73939a22e1199137db94a9cb0811a
sanoyo <sa00380304yo@gmail.com>
2023-02-19 18:10:02 +0900

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,12 @@
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 
 */
-package cmd
+package main
 
 import (
 	"os"
 
+	"github.com/sanoyo/aws-deployer/internal/cli"
 	"github.com/spf13/cobra"
 )
 
@@ -20,20 +21,19 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
+func init() {
+	rootCmd.AddCommand(cli.BuildStorageCommand())
+}
+
+func main() {
+	Execute()
+}
+
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ internal/cli/storage.go
@@ -0,0 +1,87 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cli
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+type S3Ops struct {
+	Kind       string `yaml:"kind"`
+	BucketName string `yaml:"name"`
+}
+
+func BuildStorageCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "storage",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
+		and usage of using your command. For example:
+
+		Cobra is a CLI library for Go that empowers applications.
+		This application is a tool to generate the needed files
+		to quickly create a Cobra application.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run(cmd, args)
+		},
+	}
+
+	cmd.Flags().StringP("yaml", "y", "", "Specify yaml file")
+	cmd.MarkFlagRequired("yaml")
+
+	return cmd
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	// ファイルを読み込む
+	filePath, err := cmd.Flags().GetString("yaml")
+	if err != nil {
+		return err
+	}
+
+	b, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	s3Ops := S3Ops{}
+	err = yaml.Unmarshal(b, &s3Ops)
+	if err != nil {
+		return err
+	}
+
+	// yamlファイルに読み込み
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return err
+	}
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.CreateBucket(
+		context.TODO(),
+		&s3.CreateBucketInput{
+			Bucket: aws.String(s3Ops.BucketName),
+			CreateBucketConfiguration: &types.CreateBucketConfiguration{
+				LocationConstraint: types.BucketLocationConstraint(cfg.Region),
+			},
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("output", output)
+
+	return nil
+}
